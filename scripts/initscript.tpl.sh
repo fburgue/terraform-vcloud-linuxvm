d92b147292f7bd43e44a866d90f7d4ce1c0c3adc
Jean-François Roche <jfroche@affinitic.be>
2020-02-12 00:40:05 +0100

    feat(customization): run user post script in postcustomization
    in postcustomization we have everything setup (e.g. the network) to run
    the post customization script
    
    refs CICDITS-692

--- scripts/initscript.tpl.sh
+++ scripts/initscript.tpl.sh
@@ -7,7 +7,28 @@ function create_extra_disk {
   pvcreate /dev/sdc1 2>&1
   vgextend vg_rhel /dev/sdc1 2>&1
 }
-echo "Finished setting facts"
 
-# shellcheck disable=SC2154
-${post_script}
+# shellcheck disable=SC2086
+if [ x$1 == x"precustomization" ]; then
+  echo "Started doing pre-customization steps..."
+  # shellcheck disable=SC2154
+  if [[ ${extra_disk} != 0 ]]; then create_extra_disk ; fi
+  echo "Setting facts"
+  # shellcheck disable=SC2154
+  {
+    cat <<FACTS >/etc/facter/facts.d/host-info.json
+  ${json_facts}
+FACTS
+    mkdir -p /etc/ansible/facts.d
+    cp /etc/facter/facts.d/host-info.json /etc/ansible/facts.d/cirb.fact
+    echo "environment=${hostgroup}_${zone}" >>/etc/puppetlabs/puppet/puppet.conf
+  }
+  echo "Finished setting facts"
+  echo "Finished doing pre-customization steps."
+elif [ x$1 == x"postcustomization" ]; then
+  echo "Started doing post-customization steps..."
+  # shellcheck disable=SC2154
+  ${post_script}
+  echo "Finished doing post-customization steps."
+fi
+exit 0
