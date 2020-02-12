6881f1f59dabae500e2c6c836cc08a10514913d3
Jean-François Roche <jfroche@affinitic.be>
2020-02-12 02:30:38 +0100

    fix(internal-disk): wait for disk to be created
    sometimes the creation of the extra disk can take some time in vmware.
    We wait for a while
    
    refs CICDITS-692

--- scripts/initscript.tpl.sh
+++ scripts/initscript.tpl.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 function create_extra_disk {
   echo "Adding sdc"
-  for host in /sys/class/scsi_host/*; do echo "- - -" > "$host/scan" ; done
-  for device in /sys/class/scsi_disk/*; do echo "1" > "$device/device/rescan" ; done
+  for (( i=0; i<10; ++i)); do   # wait for disk to be created (sometimes slow...)
+    for host in /sys/class/scsi_host/*; do echo "- - -" > "$host/scan" ; done
+    for device in /sys/class/scsi_disk/*; do echo "1" > "$device/device/rescan" ; done
+    fdisk -l /dev/sdc && break
+    sleep 5
+  done
   echo ',,8e;' | sfdisk /dev/sdc 2>&1
   pvcreate /dev/sdc1 2>&1
   vgextend vg_rhel /dev/sdc1 2>&1
