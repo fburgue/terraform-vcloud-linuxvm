e15be82ae864525608736f1afdfe875f3edfd715
gevraud <gevraud@cirb.brussels>
2020-11-05 10:33:30 +0100

    CICDITS-1338: fix initscript

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
-terraform {    
-  required_providers {    
-    vcd = {    
-      source  = "nixpkgs/vcd"    
-      version = "2.9.0"    
-    }    
-  }    
+terraform {
+  required_providers {
+    vcd = {
+      source  = "nixpkgs/vcd"
+      version = "2.9.0"
+    }
+  }
 }
