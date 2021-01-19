124440ce3d4dacebf0017e7f34a73f606ca69d36
Jean-François Roche <jfroche@affinitic.be>
2021-01-19 16:54:03 +0100

    fix: salt is not always in the template
    When using rhel8, we don't have salt installed by default in the
    template so the /etc/salt is not created.

--- scripts/initscript.tpl.sh
+++ scripts/initscript.tpl.sh
@@ -46,6 +46,7 @@ FACTS
   echo "Finished doing pre-customization steps."
 elif [ x$1 == x"postcustomization" ]; then
   echo "Started doing post-customization steps..."
+  mkdir -p /etc/salt
   hostname -f >/etc/salt/minion_id
   # shellcheck disable=SC2154
   ${post_script}
